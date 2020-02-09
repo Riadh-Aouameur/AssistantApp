7c664e37b9c30723d38a56018e869ed0d37143a0
Riadh-Aouameur <riadhaouameur02@gmail.com>
2020-02-09 11:43:38 +0100

    first commit

---
+++ src/main/java/MainPackage/Main.java
@@ -0,0 +1,18 @@
+package MainPackage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Assistant");
+        primaryStage.show();
+
+    }
+}
