2259c837a1b821b1bcaec2e11fb77e9416bfa149
Riadh-Aouameur <riadhaouameur02@gmail.com>
2020-02-13 02:01:03 +0100

    salle attend

---
+++ src/main/java/MainPackage/Page_3.java
@@ -0,0 +1,16 @@
+package MainPackage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Page_3 implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
